b3a4a4ddacbeaeb3097e35b9bbfeece99db000fa
Saeed <saeed.rastegar86@yahoo.de>
2020-04-20 22:37:48 +0200

    completed desktop and mobile styles

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,20 +1,8 @@
 import "../styles/styles.css";
-import Person from "./modules/Person";
+import MobileMenu from "./modules/MobileMenu";
+
+let mobileMenu = new MobileMenu();
 
 if (module.hot) {
   module.hot.accept();
 }
-
-/* Lesson example code below this line*/
-class Adult extends Person {
-  payTaxes() {
-    console.log(this.name + " now owes zero taxes.");
-  }
-}
-
-let john = new Person("John Doe", "purple");
-john.greet();
-
-let jane = new Adult("Jane Smith", "green");
-jane.greet();
-jane.payTaxes();
